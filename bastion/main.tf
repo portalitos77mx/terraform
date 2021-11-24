27836bb23adafca1545b6b94b290081fe72e6bdf
Armando <arman77mx@live.com.mx>
2021-11-24 02:13:48 -0600

    Update main.tf

--- bastion/main.tf
+++ bastion/main.tf
@@ -4,7 +4,7 @@ resource "google_compute_address" "static" {
 
 resource "google_compute_instance" "bastion_instance" {
   name         = "bastion-${var.client}-${var.environment}"
-  machine_type = "e2-micro"
+  machine_type = ${var.client}
   zone         =  "us-central1-a"  
   tags = [
     "${var.environment}-bastion-http",
